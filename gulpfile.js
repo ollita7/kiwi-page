bb598d54656bdad2e56528389c1c38ea9170c7ff
Guillermo Fernandez <guillermo.fernandez@cavepot.com>
2018-10-12 19:29:49 -0300

    Add version 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp  = require('gulp'),
-    connect  = require('gulp-connect'),
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
     less = require('gulp-less'),
     path = require('path'),
     config = require("./gulp.config"),
@@ -9,6 +9,7 @@ var gulp  = require('gulp'),
     uglify = require('gulp-uglify'),
     cleanCSS = require('gulp-clean-css'),
     concat = require('gulp-concat'),
+    watch = require('gulp-watch'),
     clean = require('gulp-clean');
 
 gulp.task("copy-html", function () {
@@ -17,19 +18,19 @@ gulp.task("copy-html", function () {
         .pipe(gulp.dest(config.dist.base));
 });
 
-gulp.task("copy-assets", function(){
+gulp.task("copy-assets", function () {
     gulp.src(config.app.assets)
         .pipe(gulp.dest(config.dist.assets));
 });
 
-gulp.task("compile-scripts", function(){
+gulp.task("compile-scripts", function () {
     gulp.src(config.app.scripts)
         .pipe(concat('scripts.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(config.dist.scripts));
 });
 
-gulp.task("compile-libs", function(){
+gulp.task("compile-libs", function () {
     gulp.src(config.app.compiledLibsStyles)
         .pipe(concat('libs.min.css'))
         .pipe(gulp.dest(config.dist.libs));
@@ -39,47 +40,57 @@ gulp.task("compile-libs", function(){
         .pipe(gulp.dest(config.dist.libs));
 })
 
- gulp.task("compile-styles", function(){
+gulp.task("compile-styles", function () {
     return gulp.src(config.app.less)
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest(config.dist.base))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(config.dist.base));
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest(config.dist.base))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest(config.dist.base));
 });
 
 /* INJECT */
 gulp.task('inject', function () {
     var target = gulp.src(config.dist.indexHtml);
-    var sources = gulp.src(config.dist.compiledSources, {read: false});
+    var sources = gulp.src(config.dist.compiledSources, { read: false });
     var stream = target.pipe(inject(sources, { relative: true }))
-              .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest("dist"));
     return stream;
 });
 
 
-gulp.task('compile', function(callback){
-  runSequence(
-            ["copy-html", "compile-libs" , "copy-assets", "compile-scripts",  "compile-styles"],
-            "inject",
-            callback);
+gulp.task('compile', function (callback) {
+    runSequence(
+        ["copy-html", "compile-libs", "copy-assets", "compile-scripts", "compile-styles"],
+        "inject",
+        callback);
 });
 
-gulp.task('clean', function(){
-    return gulp.src(config.dist.base, {read: false})
+gulp.task('clean', function () {
+    return gulp.src(config.dist.base, { read: false })
         .pipe(clean());
 })
 
-gulp.task('serve', ['compile'], function(){
-  connect.server({
-      name: 'Dev App',
-      root: ['dist', 'tmp'],
-      port: 8086,
-      livereload: true
+gulp.task('watch', function () {
+    gulp.watch(['app/**/*'], ['compile', 'reload'])
+})
+
+gulp.task('connect', function () {
+    connect.server({
+        name: 'Dev App',
+        root: ['dist', 'tmp'],
+        port: 8086,
+        livereload: true
     });
 });
 
-gulp.task('default', function() {
-  console.log('Gulp is running!')
+gulp.task('reload', function () {
+    connect.reload();
+});
+
+gulp.task('serve', ['compile', 'connect', 'watch']);
+
+gulp.task('default', function () {
+    console.log('Gulp is running!')
 });
